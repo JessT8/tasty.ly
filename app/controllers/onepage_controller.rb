9add5a1d559d8eb1bceefda78356e939fbb25a23
Jess <jessicatanhc@gmail.com>
2020-03-06 14:25:38 +0800

    Modify routes

--- app/controllers/onepage_controller.rb
+++ app/controllers/onepage_controller.rb
@@ -1,5 +1,6 @@
 class OnepageController < ApplicationController
   before_action :authenticate_user!
   def index
+      redirect_to restaurants_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_for :owners
   devise_for :users
   root 'onepage#index'
+  get '/restaurants' => 'restaurant#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
