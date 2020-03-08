c7f51006f9b5c7ac5c565c4168de4c1588932881
Jess <jessicatanhc@gmail.com>
2020-03-09 00:09:34 +0800

    Add restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -44,7 +44,7 @@ class RestaurantsController < ApplicationController
   # POST /restaurants.json
   def create
     @restaurant = Restaurant.new(restaurant_params)
-
+    @restaurant.owner_id = current_owner.id
     respond_to do |format|
       if @restaurant.save
         format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
